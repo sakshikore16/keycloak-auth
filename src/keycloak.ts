0a96336aa8ae86bd2577afed5a4a5f117c79684f
sakshi <2023.sakshik@isu.ac.in>
2025-04-27 12:23:31 +0530

    Fix Keycloak initialization and context provider

--- src/keycloak.ts
+++ src/keycloak.ts
@@ -1,16 +1,9 @@
 import Keycloak from 'keycloak-js';
 
-let keycloak: Keycloak.KeycloakInstance | null = null;
+const keycloak = new Keycloak({
+  url: 'http://localhost:8080/auth', // Keycloak server URL
+  realm: 'collab-whiteboard',        // Keycloak realm
+  clientId: 'whiteboard-frontend',   // Keycloak client ID
+});
 
-export function getKeycloak() {
-  if (typeof window !== 'undefined' && !keycloak) {
-    keycloak = new Keycloak({
-      url: 'http://localhost:8081/', // Keycloak server URL
-      realm: 'collab-whiteboard',       // Keycloak realm (updated)
-      clientId: 'whiteboard-frontend', // Keycloak client ID
-    });
-  }
-  return keycloak;
-}
-
-export default getKeycloak; 
+export default keycloak; 
